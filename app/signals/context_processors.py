f671ab8567da144f2bfc322a361688b70fa5da47
Engelbert Wijnhoven <58734663+engelbertw@users.noreply.github.com>
2026-09-09 21:25:29 +0200

    Fix comment formatting in context_processors.py

--- app/signals/context_processors.py
+++ app/signals/context_processors.py
@@ -1,5 +1,5 @@
- # SPDX-License-Identifier: MPL-2.0
-  # Copyright (C) 2019 - 2021 Gemeente Amsterdam
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2019 - 2021 Gemeente Amsterdam
   from django.conf import settings
 
 
